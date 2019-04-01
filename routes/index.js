38bf6b1097c83a8550fc187a60ea87196a7d8df7
sector <hungdbqh1710@gmail.com>
2019-04-02 03:09:53 +0700

    add router

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ const router = require('express').Router();
 const authroutes = require('./auth.routes');
 
 router.use('/auth' , authroutes);
-
+router.get('/',  (req, res) => {
+    res.send("Welcome to my vps api DA-KTHDV");
+})
 module.exports = router;
