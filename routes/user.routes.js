8df8848b00a9d9d3a7fb908944c49ceee03a8757
hung <hungdbqh1710@gmail.com>
2019-04-22 00:47:07 +0700

    update router

--- routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-var userRouter = express.Router();
-const UserController = require('../controllers/user.controller')
-
-userRouter.get('/', (req, res) => UserController.getUser(req, res));
-
-module.exports = userRouter;
