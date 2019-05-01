efa00024202b98e8775e071e2f35cb535027d06e
hung <hungdbqh1710@gmail.com>
2019-05-01 16:22:36 +0700

    updated api routes

--- routes/address.routes.js
+++ routes/address.routes.js
@@ -4,7 +4,7 @@ const AddressController = require('../controllers/address.controller');
 const { accessToken } = require('../middlewares/auth.middleware');
 
 addressRouter.get('/', (req, res) => AddressController.getAllAddress(req, res));
-addressRouter.get('/detail', (req, res) => AddressController.getDetailAddress(req, res));
+addressRouter.get('/detail/:address', (req, res) => AddressController.getDetailAddress(req, res));
 addressRouter.get('/:id', accessToken, (req, res) => AddressController.getAddressById(req, res)); // address_id
 
 module.exports = addressRouter;
